5e0248aa95e657edd6e9e99b8e5ed0fa8de771a4
Srishtivishnoi2102 <Srishtivishnoi2102@gmail.com>
2020-09-26 03:26:28 +0530

    Created customer model

---
+++ models/customer.js
@@ -0,0 +1,46 @@
+const { Sequelize , DataTypes} = require("sequelize/types");
+const { db } = require("../lib/datacentre/mysql");
+
+
+const Customer = db.define( "customer", {
+    id : {
+        type : DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey : true,
+        autoIncrement : true
+    },
+
+    email : {
+        type : DataTypes.STRING,
+        unique : true,
+        allowNull : false,
+        
+    },
+
+    name : {
+        type : DataTypes.STRING,
+        allowNull : false,
+        
+    },
+
+    dob : {
+        type : DataTypes.DATE,
+        allowNull : false,
+    },
+
+    mobileNumber : {
+        type : DataTypes.STRING(10),
+        unique : true,
+        allowNull : false,
+        
+    },
+
+    encryptedPassword : {
+        type : DataTypes.STRING,
+        allowNull : false,        
+    }
+
+})
+
+
+module.exports = Customer;
