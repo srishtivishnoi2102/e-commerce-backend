efadc84b45a366b987425cb0703404adb206c4d4
Srishtivishnoi2102 <Srishtivishnoi2102@gmail.com>
2020-09-26 03:35:31 +0530

    Created address  model

---
+++ models/address.js
@@ -0,0 +1,57 @@
+const { Sequelize , DataTypes} = require("sequelize/types");
+const { db } = require("../lib/datacentre/mysql");
+const Customer = require("./customer");
+
+
+const Address = db.define( "customer", {
+    id : {
+        type : DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey : true,
+        autoIncrement : true
+    },
+
+    customerId : {
+        type : DataTypes.INTEGER,
+        allowNull : false,
+        references : {
+            model : Customer,
+            key : 'id'
+        }
+        
+    },
+
+    fullName : {
+        type : DataTypes.STRING,
+        allowNull : false,
+        
+    },
+
+    mobileNumber : {
+        type : DataTypes.STRING(10),
+        unique : true,
+        allowNull : false,
+        
+    },
+
+    address1 : {
+        type : DataTypes.STRING,
+        allowNull : false,        
+    },
+
+    address2 : {
+        type : DataTypes.STRING,
+        allowNull : false,        
+    },
+
+    city : {
+        type : DataTypes.STRING,
+        allowNull : false,        
+    },
+
+
+
+})
+
+
+module.exports = Address;
