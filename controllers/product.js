9920f37cfb013536b77fc450f8081fe01556afcd
Srishtivishnoi2102 <Srishtivishnoi2102@gmail.com>
2020-10-08 13:46:13 +0530

    product services

--- controllers/product.js
+++ controllers/product.js
@@ -3,46 +3,28 @@ const { default: to } = require("await-to-js");
 const ProductModel = require("../lib/datacentre/models/product");
 const ProductValidator = require("../lib/PayloadValidation/product");
 
+
 const { invalidPayloadError, dbError, sendResponse } = require("../lib/utils/error_handler");
 const { isNormalInteger } = require("../lib/utils/helper");
+const ProductService = require("../services/product");
 
 const postProduct = async(req, res) => {
     let err, result;
-    let productDetails = req.body.specifications;
-    delete req.body.specifications;
 
-    [err, result]= await to(ProductValidator.newProduct.validateAsync(req.body));
+    let product = req.body;
+
+    [err, result]= await to(ProductValidator.newProduct.validateAsync(product));
     if(err){
         return invalidPayloadError(res, err);
     }
 
-    [err, result] = await to(ProductModel.findAndCountAll({
-        where : {
-            name : req.body.name,
-        }
-    }));
-
+    [err, result]= await to(ProductService.addNewProduct(product));
     if(err){
-        return dbError(res, err);
+        dbError(res,err);
     }
 
-    if (result.count){
-        return res.json({
-            success : false,
-            message : "Product already exists",
-        }); 
-   }
 
-//    insert into db
-   [err, result ] = await to(ProductModel.create(req.body));
-   if(err){
-       return dbError(res, err);
-    }
-
-    const pid = result.dataValues.id;
-
-
-   sendResponse(res, result);
+    sendResponse(res, result);
 
 }
 
@@ -50,10 +32,7 @@ const getAllProducts = async(req, res) => {
     let err, result;
 
     
-    [err, result] = await to(ProductModel.findAndCountAll({
-        attributes : {exclude : ['id'] },
-    }
-    ) );
+    [err, result] = await to(ProductService.getAllProducts());
 
     if(err){
         return dbError(res, err);
@@ -73,8 +52,15 @@ const getAllProducts = async(req, res) => {
 const getProductById = async(req, res) => {
     let err, result;
 
-    const pid = req.params.product_id;
-    [err, result ]= await to(ProductModel.findByPk(pid));
+    const productId = req.params.product_id;
+    if (! isNormalInteger(productId)){
+        return res.json({
+            success : false,
+            message : "Invalid product id",
+            data : null,
+        });
+    }
+    [err, result ]= await to(ProductService.getProductById(productId));
     
     if(err){
         return dbError(res, err);
@@ -82,21 +68,20 @@ const getProductById = async(req, res) => {
     if(!result){
         return  res.json({
             success : false, 
-            message : "NO product exists with product id "+pid,
+            message : "NO product exists with product id "+productId,
             data : null,
         });
     }
-    console.log("product with id-"+pid + result);
+
     return sendResponse(res, result);
 
 }
 
 
 const getAllProductsWithCategoryId = async(req, res) => {
-    let cid;
     
-    cid = req.params.category_id;
-    if (! isNormalInteger(cid)){
+    const categoryId = req.params.category_id;
+    if (! isNormalInteger(categoryId)){
         return res.json({
             success : false,
             message : "Invalid category id",
@@ -107,12 +92,7 @@ const getAllProductsWithCategoryId = async(req, res) => {
     
     let err, result;
 
-    [err, result ]= await to(ProductModel.findAndCountAll({
-        attributes : {exclude : ['id'] },
-        where: {
-            categoryId : cid,
-        }
-    }));
+    [err, result ]= await to(ProductService.getAllProductsWithCategoryId(categoryId));
     
     if(err){
         return dbError(res, err);
@@ -120,7 +100,7 @@ const getAllProductsWithCategoryId = async(req, res) => {
     if(result.count==0){
         return  res.json({
             success : false, 
-            message : "NO product exists with category id "+cid,
+            message : "NO product exists with category id "+categoryId,
             data : null,
         });
     }
@@ -128,19 +108,21 @@ const getAllProductsWithCategoryId = async(req, res) => {
 }
 
 const getProductDetails = async(req, res) => {
-    const pid = req.params.product_id;
+    const productId = req.params.product_id;
 
     let err, result;
-    if(!isNormalInteger(pid)){
+    if(!isNormalInteger(productId)){
         return res.json({
             success : false,
-            pid,
             message : "Invalid product id",
             data : null,
         });
     }
-
-    sendResponse(res, "Procduct Details");
+    [err, result ]=  await to(ProductService.getProductDetails(productId));
+    if(err){
+        return dbError(res, err);
+    }
+    sendResponse(res, result);
 
 
 
